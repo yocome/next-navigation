a64cf6e4358a197812a3c70c5eeb9a707f95a44a
Yocome <yocome@gmail.com>
2023-11-03 14:10:43 +0800

    fixed character error.

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import Image from "next/image";
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <h1 className="text-blue-500">I'm blue</h1>
+      <h1 className="text-blue-500">I&#39;m blue</h1>
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         {/* <AcmeLogo /> */}
       </div>
